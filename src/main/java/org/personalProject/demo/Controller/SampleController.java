ea1bb5e8aaa2544a284ced520182ecac807998e0
younjaeyeal <jyyoun1021@gmail.com>
2022-04-30 20:20:32 +0900

    샘플

--- src/main/java/org/personalProject/demo/Controller/SampleController.java
+++ src/main/java/org/personalProject/demo/Controller/SampleController.java
@@ -3,13 +3,20 @@ package org.personalProject.demo.Controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
+@RequestMapping("/sample")
 public class SampleController {
 
-    @GetMapping("/exLayout1")
+    @GetMapping({"/exLayout1","/exLayout2"})
     public void exLayout1(){
         log.info("exLayout...........");
+    }
+
+    @GetMapping("/ex1")
+    public void ex1(){
+        log.info("ex01...............");
     }
 }
